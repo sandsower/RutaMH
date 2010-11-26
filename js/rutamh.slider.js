c8791a9c0b571dd6ce00c4ece6e6a0f8649a667c
sandsowerlx <bensandsower@gmail.com>
2010-11-26 00:01:48 -0600

    Terminada Info 1, clonado el template para la informacion faltante.

---
+++ js/rutamh.slider.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('#slider1').anythingSlider({
+    width: 600,
+    heigth: 350,
+    startStopped: true,
+    resizeContent: true
+  }).anythingSliderFx({
+     '.fade' : [ 'fade' ],
+    inFx: {
+      '.textSlide h3'  : { opacity: 1, top  : 0, time: 400, easing : 'easeOutBounce' },
+      '.textSlide li'  : { opacity: 1, left : 0, time: 400 },
+      '.textSlide img' : { opacity: 1, time: 400 },
+      '.quoteSlide'    : { top : 0, time: 400, easing : 'easeOutElastic' },
+      '.expand'        : { width: '100%', top: '0%', left: '0%', time: 400, easing : 'easeOutBounce' }
+    },
+    outFx: {
+      '.textSlide h3'      : { opacity: 0, top  : '-100px', time: 350 },
+      '.textSlide li:odd'  : { opacity: 0, left : '-200px', time: 350 },
+      '.textSlide li:even' : { opacity: 0, left : '200px',  time: 350 },
+      '.textSlide img'     : { opacity: 0, time: 350 },
+      '.quoteSlide:first'  : { top : '-500px', time: 350 },
+      '.quoteSlide:last'   : { top : '500px', time: 350 },
+      '.expand'            : { width: '10%', top: '50%', left: '50%', time: 350 }
+    }
+  });
+});
+
